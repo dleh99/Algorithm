d0e5483cb33a58e9e59f6d4ae0544232d6e5edc3
Lee Do <dleh99@naver.com>
2023-10-22 16:08:16 +0900

    25083 새싹
    이게 알고리즘?

---
+++ 알고리즘/25083_새싹.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string art =
+        "         ,r'\"7\n"
+        "r`-_   ,'  ,/\n"
+        " \\. \". L_r'\n"
+        "   `~\\/ \n"
+        "      |\n"
+        "      |\n";
+
+    cout << art;
+}
